ab8b7b439844ba36a94ec55ea9296688c2c2d0cd
MiguelAtencio <49257405+MiguelAtencio@users.noreply.github.com>
2020-05-09 15:41:15 -0500

    Update DPLL.py

--- DPLL.py
+++ DPLL.py
@@ -14,7 +14,7 @@ def DPLL(S, I):
     if UP.UnitPropagate(S, I):
        S, I = UP.UnitPropagate(S, I)
 
-    if  [] in S:
+    if  '[]' in S:
         return False, {}
 
     if  len(S) == 0:
@@ -30,29 +30,20 @@ def DPLL(S, I):
             if l not in I:
                 break
 
-    print("Escogio:",l)
     new_S = set()
     for i in S:
         new_S.add(elimina_clausulas(l, i).replace(l, ''))
 
     new_I = {}
     new_I[l] = 1
-    print(l)
     if DPLL(new_S, new_I):
         new_I[l] = 0
         return True, new_I
-
+    
     else:
         new_Sv2 = set()
         for i in S:
-            if i[0] != '-':
-                l = i[0]
-                if l not in I:
-                    break
-            else:
-                l = i[1]
-                if l not in I:
-                    break
+            new_Sv2.add(elimina_clausulas(l, i).replace(l, ''))
         new_I[l] = 0
         return DPLL(new_Sv2, new_I)
 
