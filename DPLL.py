7207fada0bc92dfa3b1de01009b45c6206af7878
MiguelAtencio <49257405+MiguelAtencio@users.noreply.github.com>
2020-05-10 15:57:36 -0500

    Update DPLL.py

--- DPLL.py
+++ DPLL.py
@@ -38,7 +38,10 @@ def DPLL(S, I):
         if new_clause not in new_S and new_clause:
             new_S.append(new_clause)
 
-    I[l] = 1
+    if l[0] == '-':
+        I[lcomp] = 0
+    else:
+        I[l] = 1
     #print(I)
     res,II=DPLL(new_S,I)
     if res=="Satisfacible":
@@ -56,12 +59,15 @@ def DPLL(S, I):
                         new_clause.append(x)
             if new_clause not in new_Sv2 and new_clause:
                 new_Sv2.append(new_clause)
-        I[l] = 0
+        if l[0] == '-':
+            I[lcomp] = 1
+        else:
+            I[l] = 0
         return DPLL(new_Sv2, I)
 
 
 
 
 
-b = [['p', '-q', 'r'], ['-p', 'q', '-r'], ['-p', '-q', 'r'], ['-p', '-q', '-r']]
+b = [['r', 'p', 's'], ['-r', '-p', '-s'], ['-r', 'p', 's'], ['p', '-s']]
 print(DPLL(b, {}))
