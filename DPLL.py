3863f8c2e8ecba3236cd047cb15a7dd389367b52
MiguelAtencio <49257405+MiguelAtencio@users.noreply.github.com>
2020-05-10 15:05:37 -0500

    Update DPLL.py

--- DPLL.py
+++ DPLL.py
@@ -2,9 +2,7 @@ import UnitPropagate as UP
 
 
 def DPLL(S, I):
-
-    if UP.UnitPropagate(S, I):
-        S, I = UP.UnitPropagate(S, I)
+    S, I = UP.UnitPropagate(S, I)
 
     if  [] in S:
         return "Insatisfacible", {}
@@ -14,11 +12,13 @@ def DPLL(S, I):
 
     for i in S:
         for x in i:
-            if x not in I:
+            if x not in I and x[:0] + x[1:] not in I and '-'+x not in I:
                 l = x
                 break
         if l:
             break
+
+
 
     if l[0] != '-':
         lcomp = '-'+l
@@ -34,11 +34,11 @@ def DPLL(S, I):
                     new_clause.append(x)
         if new_clause not in new_S and new_clause:
             new_S.append(new_clause)
-
     I[l] = 1
-    print(I)
-    if DPLL(new_S, I):
-        return "Satisfacible", I
+    new_I = I.copy()
+    new_I[l] = 0
+    if DPLL(new_S, I) == ("Satisfacible", new_I):
+        return "Satisfacible", new_I
     
     else:
         new_Sv2 = []
@@ -50,8 +50,7 @@ def DPLL(S, I):
                         new_clause.append(x)
             if new_clause not in new_Sv2 and new_clause:
                 new_Sv2.append(new_clause)
-        I[l] = 0
-        return DPLL(new_Sv2, I)
+        return DPLL(new_Sv2, new_I)
 
 
 
